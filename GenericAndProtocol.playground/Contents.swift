73eac0b2ae98896e6ef46d7752706b8b7c88c1f1
Junliang.Song <songjl1@yonghui.cn>
2021-09-27 19:25:36 +0800

    feat: edit keynote

--- GenericAndProtocol.playground/Contents.swift
+++ GenericAndProtocol.playground/Contents.swift
@@ -69,4 +69,85 @@ contactListView.items = [
 contactListView.selectionHandler = {
     print("\($0),\($1.name)")
 }
+
 PlaygroundPage.current.liveView = contactListView
+
+enum Event {
+    case pageShow(param: [String: Any]?)
+    case pageLeave(param: [String: Any]?)
+    case click(param: [String: Any]?)
+    case expro(param: [String: Any]?)
+    var data: [String: Any] {
+        var data = [String: Any]()
+        switch self {
+        case .pageShow(let param):
+            data["event"] = "pageShow"
+            data["param"] = param
+        case .pageLeave(let param):
+            data["event"] = "pageLeave"
+            data["param"] = param
+        case .expro(let param):
+            data["event"] = "expro"
+            data["param"] = param
+        case .click(let param):
+            data["event"] = "click"
+            data["param"] = param
+        }
+        return data
+    }
+}
+
+protocol Trackable {
+    var pageName: String? { get set }
+}
+
+extension Trackable {
+    func track(event: Event) {
+        print(self.pageName ?? "")
+        print(event.data)
+    }
+}
+
+public class Tracker<Base: AnyObject>: Trackable {
+    public weak var base: Base?
+    public var pageName: String?
+    fileprivate init(_ base: Base) {
+        self.base = base
+        if let vc = base as? UIViewController {
+            self.pageName = vc.title
+        }
+
+        if let view = base as? UIView {
+            var responder = view.next
+            repeat {
+                responder = responder?.next
+            } while !(responder?.next is UIViewController)
+            if let vc = responder as? UIViewController {
+                self.pageName = vc.tracker.pageName
+            }
+        }
+    }
+}
+
+private var trackerKey = ".trackerKey"
+public protocol TrackerProvider: AnyObject {}
+public extension TrackerProvider {
+    var tracker: Tracker<Self> {
+        var tracker = objc_getAssociatedObject(self, &trackerKey) as? Tracker<Self>
+        if tracker == nil {
+            tracker = Tracker(self)
+            objc_setAssociatedObject(self, &trackerKey, tracker, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+        return tracker!
+    }
+}
+
+extension UIViewController: TrackerProvider {}
+public extension Tracker where Base: UIViewController {}
+
+extension UIView: TrackerProvider {}
+public extension Tracker where Base: UIView {}
+
+contactListView.title = "联系人列表"
+contactListView.tracker.track(event: .pageShow(param: nil))
+// contactListView.tableView.tracker.track(event: .pageShow(param: nil))
--- ResultBuilder.playground/Contents.swift
+++ ResultBuilder.playground/Contents.swift
@@ -1,42 +1,43 @@
 //: A UIKit based Playground for presenting user interface
-  
-import UIKit
+
 import PlaygroundSupport
+import UIKit
 
 @resultBuilder
-struct AttributedStringBuilder {
-  static func buildBlock(_ segments: NSAttributedString...) -> NSAttributedString {
-    let string = NSMutableAttributedString()
-    segments.forEach { string.append($0) }
-    return string
-  }
+enum AttributedStringBuilder {
+    static func buildBlock(_ segments: NSAttributedString...) -> NSAttributedString {
+        let string = NSMutableAttributedString()
+        segments.forEach { string.append($0) }
+        return string
+    }
 }
 
 extension NSAttributedString {
-  convenience init(@AttributedStringBuilder _ content: () -> NSAttributedString) {
-    self.init(attributedString: content())
-  }
+    convenience init(@AttributedStringBuilder _ content: () -> NSAttributedString) {
+        self.init(attributedString: content())
+    }
 }
 
-class MyViewController : UIViewController {
+class MyViewController: UIViewController {
     override func loadView() {
         let view = UIView()
         view.backgroundColor = .white
 
         let label = UILabel()
         label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        
+
         label.attributedText = NSAttributedString {
             "Hello "
-              .foregroundColor(.red)
-              .font(UIFont.systemFont(ofSize: 10.0))
+                .foregroundColor(.red)
+                .font(UIFont.systemFont(ofSize: 10.0))
             "World"
-              .foregroundColor(.green)
-              .underline(.orange, style: .thick)
+                .foregroundColor(.green)
+                .underline(.orange, style: .thick)
         }
         view.addSubview(label)
         self.view = view
     }
 }
+
 // Present the view controller in the Live View window
 PlaygroundPage.current.liveView = MyViewController()
